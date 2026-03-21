16bff666c6599fad0132ae8df1860fc0343d7024
_ <_@_._>
2026-03-21 19:17:34 +0300

    #208

--- main/save_trades/schemas.py
+++ main/save_trades/schemas.py
@@ -36,40 +36,6 @@ class OKXTradeDataOld(Base):
     __tablename__ = 'okx_trade_data_old'
     __table_args__ = (
         PrimaryKeyConstraint(  # Explicitly define composite primary key
-            'symbol_name',
-            'trade_id',
-        ),
-    )
-
-    # Primary key fields
-
-    symbol_name: Mapped[str] = Column(Text)
-    trade_id: Mapped[int] = Column(BigInteger)
-
-    # Attribute fields
-
-    is_buy: Mapped[bool] = Column(Boolean)
-
-    price: Mapped[Decimal] = Column(Numeric)
-    quantity: Mapped[Decimal] = Column(Numeric)
-
-    timestamp_ms: Mapped[int] = Column(BigInteger)
-
-    # Indices
-
-    timestamp_ms_idx = (
-        Index(
-            'timestamp_ms_idx',
-            'symbol_name',
-            'timestamp_ms',
-        ),
-    )
-
-
-class OKXTradeDataOld2(Base):
-    __tablename__ = 'okx_trade_data_old2'
-    __table_args__ = (
-        PrimaryKeyConstraint(  # Explicitly define composite primary key
             'symbol_id',
             'trade_id',
         ),
